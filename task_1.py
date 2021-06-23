665e303af0839cd22e0af882ab939f13c3b10ab9
ladudanil <danlad2022@gmail.com>
2021-06-23 16:18:19 +0300

    Delete task_1.py

--- task_1.py
+++
@@ -1,7 +0,0 @@
-from scrapy import cmdline
-from lxml import etree
-
-cmdline.execute("scrapy crawl xsport".split())
-root = None
-with open('results/xsport.xml', 'r') as file:
-    root = etree.parse(file)
